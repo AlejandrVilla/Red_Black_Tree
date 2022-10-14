f34b5db64f744c5395379ee349a6f2aa2b0b0279
AlejandrVilla <avillah@unsa.edu.pe>
2022-10-14 18:04:33 -0500

    .

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include "RB_tree.h"
 using namespace std;
 
+void guardar(vector<pair<int,int>>& V, string dir)
+{
+    ofstream f(dir);
+    for(auto a : V)
+        f<<a.first<<' '<<a.second<<'\n';
+    f.close();
+}
+
 int main()
 {
     // RB_Tree<int> RB;
@@ -16,18 +24,18 @@ int main()
     RB_Tree<int> RB;
     vector<pair<int,int>> res;
     int n = pow(10,2);
-    for( ; n<10000 ; n*=2)
+    for( ; n<=10000 ; n+=100)
     {
         srand(time(NULL));
         for(int j=n ; j>0 ; --j)
             RB.insert(j);
         //variable = limite_inferior + rand() % (limite_superior +1 - limite_inferior);
-        int num = 1 + rand() % n;
-        // int num = n;
-        int cant = RB.search(num);
-        res.push_back(make_pair(n,cant));
-        cout<<"valor buscado "<<num<<" tamanio "<<RB.getSize()<<" -> comparaciones "<<cant<<'\n';
+        int num = 1 + rand() % (n/10);
+        // int num = 50;
+        int comp = RB.search(num);
+        res.push_back(make_pair(n,comp));
+        // cout<<"valor buscado "<<num<<" tamanio "<<RB.getSize()<<" -> comparaciones "<<comp<<'\n';
         RB.clear();
     }
-
+    guardar(res,"data.txt");
 }
