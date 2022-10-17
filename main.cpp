e5d9b79ea05ccad6735e12753a116ef266a21fac
JulioYauri <83053778+JulioYauri@users.noreply.github.com>
2022-10-16 19:20:10 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,30 +12,30 @@ void guardar(vector<pair<int,int>>& V, string dir)
 
 int main()
 {
-    // RB_Tree<int> RB;
-    // for(int i=20 ; i>0 ; --i)
-    //     RB.insert(i);
-    // RB.inOrder();
-    // RB.preOrder();
-    // RB.postOrder();
-    // cout<<RB.search(20)<<'\n';
-    // cout<<RB.search(1)<<'\n';
-    // RB.clear();
     RB_Tree<int> RB;
-    vector<pair<int,int>> res;
-    int n = pow(10,2);
-    for( ; n<=10000 ; n+=100)
-    {
-        srand(time(NULL));
-        for(int j=n ; j>0 ; --j)
-            RB.insert(j);
-        //variable = limite_inferior + rand() % (limite_superior +1 - limite_inferior);
-        int num = 1 + rand() % (n/10);
-        // int num = 50;
-        int comp = RB.search(num);
-        res.push_back(make_pair(n,comp));
-        // cout<<"valor buscado "<<num<<" tamanio "<<RB.getSize()<<" -> comparaciones "<<comp<<'\n';
-        RB.clear();
-    }
-    guardar(res,"data.txt");
+    for(int i=1 ; i <= 20 ; ++i)
+        RB.insert(i);
+    RB.inOrder();
+    RB.preOrder();
+    RB.postOrder();
+    cout<<RB.search(20)<<'\n';
+    cout<<RB.search(1)<<'\n';
+    RB.clear();
+    // RB_Tree<int> RB;
+    // vector<pair<int,int>> res;
+    // int n = pow(10,2);
+    // for( ; n<=10000 ; n+=100)
+    // {
+    //     srand(time(NULL));
+    //     for(int j=n ; j>0 ; --j)
+    //         RB.insert(j);
+    //     //variable = limite_inferior + rand() % (limite_superior +1 - limite_inferior);
+    //     int num = 1 + rand() % (n/10);
+    //     // int num = 50;
+    //     int comp = RB.search(num);
+    //     res.push_back(make_pair(n,comp));
+    //     // cout<<"valor buscado "<<num<<" tamanio "<<RB.getSize()<<" -> comparaciones "<<comp<<'\n';
+    //     RB.clear();
+    // }
+    // guardar(res,"data.txt");
 }
